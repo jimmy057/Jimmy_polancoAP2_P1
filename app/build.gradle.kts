6a63ffef4108a660c2a8d4aaa644a1fcc73fde11
jimmy057 <jimmypolanco2540@gmail.com>
2025-10-02 03:02:20 -0400

    modificacion en el build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.registrojugadores"
+    namespace = "com.example.jimmy_polancoap2_p1"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.registrojugadores"
+        applicationId = "com.example.jimmy_polancoap2_p1"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.firebase.crashlytics.buildtools)
     implementation(libs.androidx.databinding.adapters)
     implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.material3)
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
 
@@ -67,7 +68,7 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.compose.material3:material3:1.1.0-alpha02")
     implementation ("androidx.compose.material:material:1.4.0")
-    implementation("androidx.compose.material:material:1.3.1") // Usa la última versión
+    implementation("androidx.compose.material:material:1.3.1")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
