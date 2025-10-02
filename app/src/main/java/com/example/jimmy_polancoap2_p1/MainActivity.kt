0ea8bef608465f5cd09df5c0ec397e5e1a48321b
jimmy057 <jimmypolanco2540@gmail.com>
2025-10-02 03:03:45 -0400

    modificacion en el MainActivity.kt

--- app/src/main/java/com/example/jimmy_polancoap2_p1/MainActivity.kt
+++ app/src/main/java/com/example/jimmy_polancoap2_p1/MainActivity.kt
@@ -7,41 +7,59 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.jimmy_polancoap2_p1.presentacion.edit.HuacalEditScreen
+import com.example.jimmy_polancoap2_p1.presentacion.list.HuacalListScreen
 import com.example.jimmy_polancoap2_p1.ui.theme.Jimmy_polancoAP2_P1Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Jimmy_polancoAP2_P1Theme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyApp() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Jimmy_polancoAP2_P1Theme {
-        Greeting("Android")
+    Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
+        NavHost(
+            navController = navController,
+            startDestination = "list",
+            modifier = Modifier.padding(padding).fillMaxSize()
+        ) {
+            composable("list") {
+                HuacalListScreen(
+                    onEdit = { id -> navController.navigate("edit/$id") },
+                    onAdd = { navController.navigate("edit/-1") }
+                )
+            }
+
+            composable(
+                route = "edit/{id}",
+                arguments = listOf(navArgument("id") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val id = backStackEntry.arguments?.getInt("id") ?: -1
+                HuacalEditScreen(
+                    huacalId = if (id == -1) null else id,
+                    onSave = { navController.popBackStack() },
+                    onCancel = { navController.popBackStack() }
+                )
+            }
+        }
     }
 }
