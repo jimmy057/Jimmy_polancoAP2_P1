121399273295120255eb898e636d1d6d2ce29872
jimmy057 <jimmypolanco2540@gmail.com>
2025-10-02 03:05:19 -0400

    modificacion en el huacales.kt

--- app/src/main/java/com/example/jimmy_polancoap2_p1/domain/model/huacales.kt
+++ app/src/main/java/com/example/jimmy_polancoap2_p1/domain/model/huacales.kt
@@ -1,10 +1,9 @@
 package com.example.jimmy_polancoap2_p1.domain.model
 
 data class Huacal(
-    val idEntrada: Int = 0,
+    val id: Int = 0,
     val fecha: String,
-    val nombreCliente: String,
+    val cliente: String,
     val cantidad: Int,
     val precio: Double
 )
-
